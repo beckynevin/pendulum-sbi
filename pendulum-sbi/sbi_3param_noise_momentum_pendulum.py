22b730bd9f7fe8b0e35f7f4688cb40d4ecec80b6
beckynevin <beckynevin@gmail.com>
2023-02-01 16:36:39 -0700

    commented, there's still a bug with the MSE SBI run

--- pendulum-sbi/sbi_3param_noise_momentum_pendulum.py
+++ pendulum-sbi/sbi_3param_noise_momentum_pendulum.py
@@ -1,6 +1,5 @@
 # Trying this with three params now
 # also adding in noise
-
 # but write so that it includes momenta as well
 
 import torch
@@ -19,18 +18,21 @@ from sbi.inference import SNPE, prepare_for_sbi, simulate_for_sbi
 from sbi.utils.get_nn_models import posterior_nn
 from sbi.analysis import pairplot
 
-noiz = [0.1,0.0,0.0] # only noise on g
-
-# num_dim is the number of parameters were controlling (g, L, )
-num_dim = 4
-prior = utils.BoxUniform(low=-2 * torch.ones(num_dim), high=2 * torch.ones(num_dim))
-
-
+# setting the sigmas on the noise distribution
+# you will eventually draw the parameter values from a normal
+# distribution with the width given by these values
+noiz = [0.1,0.0,0.0] # here, there's only noise on g
 
 
 # This is the simulator
+# Given thetas, it outputs the x and y position (cartesian)
+# of the pendulum over a range of times
+# p and q are position and momentum
+
 # Option is to input the width of the noise normal distributions
 # around each parameter.
+# default is a bit of noise for each of the three parameters
+
 def create_t_p_q_noise(theta, noise=[0.1,0.1,0.1]):
     """
     Return an t, x, y array for plotting based on params
@@ -39,7 +41,9 @@ def create_t_p_q_noise(theta, noise=[0.1,0.1,0.1]):
 
     
 
-    # Decide on time
+    # Decide on time, here I'm sampling a few oscillations, but this could be
+    # something you import or change
+    
     t = np.linspace(0, 10, 200)
     ts = np.repeat(t[:, np.newaxis], theta.shape[0], axis=1)
 
@@ -47,38 +51,24 @@ def create_t_p_q_noise(theta, noise=[0.1,0.1,0.1]):
     if theta.ndim == 1:
         theta = theta[np.newaxis, :]
     
-    # Draw parameter (theta) values from normal distributions
-    # To produce noise
+    # time to solve for position and velocity
 
-    
-
-    # Right now, let's do this only once for each pendulum
-    # So thetas will have the same value for all t
-    # But I'm wondering if we eventually want there to be different
-    # theta values also for every moment in time, this could be like systematics?
-
-    thetas_noisy = np.random.normal(loc=theta, scale=noise, size=np.shape(theta))
-    print(np.shape(theta), np.shape(thetas_noisy))
-    print(theta)
-    print(thetas_noisy)
-    
-
-    # Okay from this then write q and p
-
-    # theta_t = thetas_noisy[:,2] * math.cos(np.sqrt(thetas_noisy[:,0] / thetas_noisy[:,1]) * t)
-    
-   
-
-    #if theta.shape[0] > 2:
-        
-        
-    # nested for loop
+    # nested for loop, there's probably a better way to do this
     # output needs to be (n,len(t))
     x = np.zeros((theta.shape[0],len(t)))
     y = np.zeros((theta.shape[0],len(t)))
+
+    # TO DO: I'm not strictly solving for momentum, just velocities:
     dx_dt = np.zeros((theta.shape[0],len(t)))
     dy_dt = np.zeros((theta.shape[0],len(t)))
     for n in range(theta.shape[0]):
+
+        # Draw parameter (theta) values from normal distributions
+        # To produce noise in the thetas you are using to produce the position
+        # and momentum of the pendulum at each moment in time
+        # Another way to do this would be to just draw once and use that same noisy theta 
+        # value for all moments in time, but this would be very similar to just drawing
+        # from the prior, which we're already doing.
     
         gs = np.random.normal(loc=theta[n][0], scale=noise[0], size=np.shape(t))
         Ls = np.random.normal(loc=theta[n][1], scale=noise[1], size=np.shape(t))
@@ -114,6 +104,13 @@ def create_t_p_q_noise(theta, noise=[0.1,0.1,0.1]):
 
     return t, x, y, dx_dt, dy_dt
 
+# This is a wrapper for the SBI that allows it use the position and momentum at all points in time
+# as its optimization target
+# i.e., its trying to optimize its hyperparameters so that the given a theta input the xs 
+# (xs == position and momentum at all moments in time) match
+# that of the simulator
+
+# TO DO: probably could just use the above function and get rid of time
 
 def eval_for_all_time(theta, noise):
     """Evaluate the pendulum at all times"""
@@ -140,7 +137,7 @@ def eval_for_all_time(theta, noise):
     
     return x, y, dx_dt, dy_dt
 
-
+# Similar to the above but draws at one specific moment in time
 def eval(theta, t, noise):
     """
     Evaluate the pendulum at time `t`
@@ -175,6 +172,10 @@ def eval(theta, t, noise):
 
     return x, y, dx_dt, dy_dt
 
+# MSE = mean squared error
+# This is another tool for the SBI to use to optimize
+# Given a noisy pendulum SBI run, it compares the true position and velocities
+# to those predicted by the SBI
 def get_MSE(theta, theta_o, noise = [0.1,0.1,0.1]):
     """
     Return the mean-squared error (MSE) i.e. Euclidean distance from the observation function
@@ -183,13 +184,13 @@ def get_MSE(theta, theta_o, noise = [0.1,0.1,0.1]):
     _, x_, y_, mom_x_, mom_y_ = create_t_p_q_noise(theta, noise = noise)  # simulations
     return np.mean(np.sqrt(np.square(y_ - y) + np.square(x_ - x) + np.square(mom_y_ - mom_y) + np.square(mom_x_ - mom_x)), axis=0, keepdims=True).T  # MSE
 
+# Another SBI optimization tool, this uses the position and velocities at multiple specific moments in time
+# TO DO: It is no longer drawing only four values
 def get_4_values(theta, noise = [0.1,0.1,0.1]):
     """
     Return 4 'y' values corresponding to t=-0.5,0,0.75 as summary statistic vector
     """
 
-    print('shape of x', np.shape(eval(theta, 0, noise = noise)[0]))
-    
     return np.array(
         [
             eval(theta, 0, noise = noise)[0],
@@ -211,13 +212,16 @@ def get_4_values(theta, noise = [0.1,0.1,0.1]):
         ]
     ).T
 
-
-# Following this example: 
+# The next step is to implement SBI using the Macklab example from:
 # https://github.com/mackelab/sbi/blob/main/tutorials/10_crafting_summary_statistics.ipynb
-# Also could look at Miles' example:
+
+# Also could look at Miles' example for reference:
 # file:///Users/rnevin/Zotero/storage/L52NCDXF/simulation-based-inference.html
 # Now redo with the pendulum simulator
-# params are g and L and theta_not
+
+# params are g and L and theta_not, which is the initial theta position
+# Here, I'm setting up the min and max on the parameters for an Uniform distribution
+# The "true" values for this experiment sit right in the middle.
 prior_min = [5, 0, 0] # range on all of the params
 prior_max = [15, 10, np.pi/2]
 
@@ -225,7 +229,14 @@ prior = utils.torchutils.BoxUniform(
     low=torch.as_tensor(prior_min), high=torch.as_tensor(prior_max)
 )
 
+# TO DO: terrible notation flying around, theta_o is referring to
+# parameters and to the starting theta of the pendulum
+
+# True parameter values
 theta_o = np.array([10, 5, np.pi/4])
+
+
+'''
 t, x, y, mom_x, mom_y = create_t_p_q_noise(theta_o, noise = noiz)
 fig = plt.figure(figsize = (10,4))
 #fig.title('True values')
@@ -254,6 +265,7 @@ ax1.set_xlabel('y pos')
 ax1.set_ylabel('momentum in y')
 
 plt.show()
+'''
 
 
 
@@ -262,33 +274,34 @@ fig = plt.figure(figsize = (10,4))
 
 ax = fig.add_subplot(121)
 
+# First, visualize by running these true thetas through the simulator with some
+# noise and plotting the x, y, v_x, v_y
 t, x_truth, y_truth, mom_x_truth, mom_y_truth = create_t_p_q_noise(theta_o, noise = noiz)
 
 ax.plot(t, x_truth[0,:], "k", zorder=1, label="truth")
-n_samples = 10
+
+# Also make 1000 draws from the priors and get the position and velocities from these
+n_samples = 100
 theta = prior.sample((n_samples,))
-print('input shape of theta', np.shape(theta))
+
 t, x, y, mom_x, mom_y = create_t_p_q_noise(theta.numpy(), noise = noiz)
-print('shape t', np.shape(t))
-print('shape x', np.shape(x))
 
-# maybe rearrange by theta value
-print(np.shape(t), np.shape(x), np.shape(theta.numpy()))
-# okay so there are 10 samples of the posterior
-# and 200 points in time to measure
 
+# If there are a bunch of samples, only plot a few of them
 if n_samples > 99:
-    ax.plot(t, x, "grey", zorder=0)
+    for i in range(10):
+        if i == 0:
+            im = ax.plot(t, x[i,:], label=str(theta.numpy()[i]), color = 'grey')#c = matplotlib.cm.hot(theta.numpy()[i,0]))
+        else:
+            im = ax.plot(t, x[i,:], color = 'grey')# don't label most
 else:
     for i in range(n_samples):
         if i == 0:
-            im = ax.plot(t, x[i,:], label=str(theta.numpy()[i]))#c = matplotlib.cm.hot(theta.numpy()[i,0]))
+            im = ax.plot(t, x[i,:], label=str(theta.numpy()[i]), color = 'grey')#c = matplotlib.cm.hot(theta.numpy()[i,0]))
         else:
-            im = ax.plot(t, x[i,:])# don't label most
+            im = ax.plot(t, x[i,:], color = 'grey')# don't label most
 
-# was: ax.plot(t, x, "grey", zorder=0)
-#im = ax.plot(t, x, c=matplotlib.cm.hot(theta.numpy()[:,0]), zorder=0)
-#plt.colorbar(im, label='g value')
+
 plt.legend()
 ax.set_xlabel('time')
 ax.set_ylabel('x')
@@ -297,21 +310,24 @@ ax1 = fig.add_subplot(122)
 ax1.plot(t, y_truth[0,:], "k", zorder=1, label="truth")
 
 if n_samples > 99:
-    ax1.plot(t, y, "grey", zorder=0)
+    for i in range(10):
+        if i == 0 :
+            im = ax1.plot(t, y[i,:], label=str(theta.numpy()[i]), color = 'grey')#c = matplotlib.cm.hot(theta.numpy()[i,0]))
+        else:
+            im = ax1.plot(t, y[i,:], color = 'grey')# don't label most
 else:
     for i in range(n_samples):
         if i == 0 :
-            im = ax1.plot(t, y[i,:], label=str(theta.numpy()[i]))#c = matplotlib.cm.hot(theta.numpy()[i,0]))
+            im = ax1.plot(t, y[i,:], label=str(theta.numpy()[i]), color = 'grey')#c = matplotlib.cm.hot(theta.numpy()[i,0]))
         else:
-            im = ax1.plot(t, y[i,:])# don't label most
+            im = ax1.plot(t, y[i,:], color = 'grey')# don't label most
 
-#ax1.plot(t, y, "grey", zorder=0)
 plt.legend()
 ax1.set_xlabel('time')
 ax1.set_ylabel('y')
 plt.show()
 
-# Do this but for momentum
+# Same thing for velocity, which again, we're calling momentum
 plt.clf()
 fig = plt.figure(figsize = (10,4))
 
@@ -327,12 +343,7 @@ else:
 
     for i in range(n_samples):
         im = ax.plot(x[i,:], mom_x[i,:])
-        #, label=str(theta.numpy()[i]))#c = matplotlib.cm.hot(theta.numpy()[i,0]))
-
-
-# was: ax.plot(t, x, "grey", zorder=0)
-#im = ax.plot(t, x, c=matplotlib.cm.hot(theta.numpy()[:,0]), zorder=0)
-#plt.colorbar(im, label='g value')
+    
 plt.legend()
 ax.set_xlabel('x')
 ax.set_ylabel('mom x')
@@ -345,32 +356,46 @@ if n_samples > 99:
 else:
     for i in range(n_samples):
         im = ax1.plot( y[i,:], mom_y[i,:])
-        #, label=str(theta.numpy()[i]))#c = matplotlib.cm.hot(theta.numpy()[i,0]))
-
-
-#ax1.plot(t, y, "grey", zorder=0)
+     
 plt.legend()
 ax1.set_xlabel('y')
 ax1.set_ylabel('mom y')
 plt.show()
 
 
+# Time to get into SBI
 
+# First, experiment with the summary statistic of drawing multiple (but not all)
+# moments in time
 
-# Lets see how the four values do
+# x will be the 'connection point' that the SBI is trying to connect with the 
+# thetas
 x = get_4_values(theta.numpy(), noise = noiz)
 x = torch.as_tensor(x, dtype=torch.float32)
 
-
+# call sequential neural posterior estimation
 inference = SNPE(prior)
 
+# Now give it what you want, which is a tool that will connect the summary
+# statistic with thetas
+# and train
 _ = inference.append_simulations(theta, x).train()
+
+# from that build a posterior
 posterior = inference.build_posterior()
 
+# get the true summary statistical from the true thetas (with noise)
 x_o = torch.as_tensor(get_4_values(theta_o, noise = noiz), dtype=float)
 
-theta_p = posterior.sample((10000,), x=x_o)
+# the nice thing about SBI (or one of the nice things)
+# is that you can sample from the posterior a bunch of times
+# without re-running the training
+# so to get the probability distribution of thetas, sample a bunch at the 
+# true x values
+theta_p = posterior.sample((n_samples,), x=x_o)
 
+# this will make a corner plot that shows the true values of the parameters
+# and the SBI solution
 fig, axes = pairplot(
     theta_p,
     limits=list(zip(prior_min, prior_max)),
@@ -383,9 +408,12 @@ fig, axes = pairplot(
 )
 plt.show()
 
+# Continuing on with this same solution, let's plot the time evolution
+# for the 10 highest probability draws
+# and the 10 worst
 x_o_t, x_o_x, x_o_y, x_o_mom_x, x_o_mom_y = create_t_p_q_noise(theta_o)
 plt.plot(x_o_t, x_o_x[0,:], "k", zorder=1, label="truth")
-theta_p = posterior.sample((100,), x=x_o)
+theta_p = posterior.sample((n_samples,), x=x_o)
 ind_10_highest = np.argsort(np.array(posterior.log_prob(theta=theta_p, x=x_o)))[-10:]
 theta_p_considered = theta_p[ind_10_highest, :]
 x_t, x_x, x_y, x_mom_x, x_mom_y = create_t_p_q_noise(theta_p_considered.numpy())
@@ -415,13 +443,10 @@ plt.show()
 # MSE of the positions produced from that prior
 # to the acutal positions at all of the times
 # (MSE samples at all times)
-theta = prior.sample((1000,))
-# These are the losses
+theta = prior.sample((n_samples,))
+# Another way to think about x is that its the loss
 x = get_MSE(theta.numpy(), theta_o, noise = noiz)
 
-print('actual theta', theta_o)
-print('thetas', theta)
-print('MSE', x)
 
 theta = torch.as_tensor(theta, dtype=torch.float32)
 x = torch.as_tensor(x, dtype=torch.float32)
@@ -446,7 +471,7 @@ x_o = torch.as_tensor(
 )
 
 # fills empty tensor with samples from the posterior
-theta_p = posterior.sample((10000,), x=x_o)
+theta_p = posterior.sample((n_samples,), x=x_o)
 
 fig, axes = pairplot(
     theta_p,
@@ -478,4 +503,4 @@ plt.ylabel('mom in x')
 plt.legend()
 plt.show()
 
-## MSE does better
+## MSE does better, which makes sense because its using more info
